3ca1743f1fd4f17cda0100bfce82231ebb64cc5f
preetha548 <vaishnavihiteash@gmail.com>
2026-01-03 00:29:45 +0530

    Task List App

--- app.js
+++ app.js
@@ -1,15 +1,147 @@
-let cnt1 = 1000;
-let cnt2 = 100;
-let cnt3 = 111;
-function followersFn() {
-	cnt1 += 1;
-	document.getElementById('followersCount').innerText = cnt1;
+import { renderTask, renderFooter } from "./library/index.js";
+
+let tasks = [];
+export let filter = "all";
+
+export const TaskStatus = {
+  TODO: "todo",
+  COMPLETED: "completed"
+};
+
+const $form = document.getElementById("form");
+const $input = document.getElementById("task");
+const $taskList = document.getElementById("task-list");
+
+$form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const task = $input.value;
+
+  if (!task) {
+    alert("Per favore, inserisci un task prima di aggiungerlo");
+    return null;
+  }
+
+  filter = "all";
+  tasks = createTask(tasks, task);
+  renderTasks(tasks);
+  renderCounter(tasks);
+
+  $input.value = "";
+});
+
+function renderTasks(taskList = []) {
+  $taskList.innerHTML = "";
+
+  taskList.forEach((task) => {
+    const $task = renderTask(task);
+    $taskList.insertAdjacentHTML("beforeend", $task);
+  });
+
+  if (!tasks?.length) return;
+
+  const $footer = renderFooter(taskList, filter);
+  $taskList.insertAdjacentHTML("beforeend", $footer);
+
+  const $filters = document.querySelector(".filters");
+  const $all = $filters.querySelector(".all");
+  const $completed = $filters.querySelector(".completed");
+  const $uncompleted = $filters.querySelector(".uncompleted");
+  const $clear = document.querySelector(".clear");
+
+  $all.addEventListener("click", () => {
+    filter = "all";
+    renderTasks(tasks);
+    renderCounter(tasks);
+  });
+
+  $completed.addEventListener("click", () => {
+    filter = "completed";
+    const completedTasks = getCompletedTasks(tasks);
+    renderTasks(completedTasks);
+    renderCounter(tasks);
+  });
+
+  $uncompleted.addEventListener("click", () => {
+    filter = "todo";
+    const uncompletedTasks = getUncompletedTasks(tasks);
+    renderTasks(uncompletedTasks);
+    renderCounter(tasks);
+  });
+
+  $clear.addEventListener("click", () => {
+    tasks = [];
+    renderTasks(tasks);
+    renderCounter(tasks);
+  });
+
+  watchTasks();
 }
-function followingFn() {
-	cnt2 += 1;
-	document.getElementById('followingCount').innerText = cnt2;
+
+function watchTasks() {
+  const $tasks = document.querySelectorAll(".task");
+
+  $tasks.forEach(($task) => {
+    const id = $task.getAttribute("id");
+    const $toggleTask = $task.querySelector(".toggle");
+    const $deleteTask = $task.querySelector(".delete-task");
+
+    $toggleTask.addEventListener("click", () => {
+      tasks = toggleTaskStatus(tasks, id);
+      renderTasks(tasks);
+      renderCounter(tasks);
+    });
+
+    $deleteTask.addEventListener("click", () => {
+      tasks = deleteTask(tasks, id);
+      renderTasks(tasks);
+      renderCounter(tasks);
+    });
+  });
 }
-function articlesFn() {
-	cnt3 += 1;
-	document.getElementById('articlesCount').innerText = cnt3;
+
+// Item Actions
+function createTask(tasks, title) {
+  const newTask = {
+    id: Date.now(),
+    title,
+    status: TaskStatus.TODO
+  };
+
+  tasks.unshift(newTask);
+
+  return tasks;
+}
+
+function toggleTaskStatus(tasks, id) {
+  const foundTask = findTask(tasks, id);
+  const isCompleted = foundTask.status === TaskStatus.COMPLETED;
+
+  foundTask.status = isCompleted ? TaskStatus.TODO : TaskStatus.COMPLETED;
+
+  return tasks;
+}
+
+function findTask(tasks, id) {
+  return tasks.find((task) => task.id === Number(id));
+}
+
+function getCompletedTasks(tasks) {
+  return tasks.filter((task) => task.status !== TaskStatus.TODO);
+}
+
+function getUncompletedTasks(tasks) {
+  return tasks.filter((task) => task.status !== TaskStatus.COMPLETED);
+}
+
+function deleteTask(tasks, id) {
+  return tasks.filter((task) => task.id !== Number(id));
+}
+
+// Render counter
+function renderCounter(tasks) {
+  const $counter = document.createElement("div");
+  $counter.classList.add("counter");
+  $counter.innerHTML = `${tasks.length} tasks totali`;
+
+  $taskList.append($counter);
 }
