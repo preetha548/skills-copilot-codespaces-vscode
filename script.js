c66057cc8d10a8c55c9af867bf1669830ea04817
preetha548 <vaishnavihiteash@gmail.com>
2026-01-02 22:58:09 +0530

    Online Auction

--- script.js
+++ script.js
@@ -1,27 +1,9 @@
-const medicationForm = document.getElementById('medication-form');
-medicationForm.addEventListener('submit', addMedication);
-
-function addMedication(e) {
-    e.preventDefault();
-    const medicationName = document.getElementById('medication-name').value;
-    const medicationDosage = document.getElementById('medication-dosage').value;
-    const medicationList = document.getElementById('medication-list');
-    const li = document.createElement('li');
-    li.innerText = `${medicationName} - ${medicationDosage}`;
-    medicationList.appendChild(li);
-    medicationForm.reset();
-}
-
-const reminderForm = document.getElementById('reminder-form');
-reminderForm.addEventListener('submit', addReminder);
-
-function addReminder(e) {
-    e.preventDefault();
-    const reminderName = document.getElementById('reminder-name').value;
-    const reminderTime = document.getElementById('reminder-time').value;
-    const reminderList = document.getElementById('reminder-list');
-    const li = document.createElement('li');
-    li.innerText = `${reminderName} - ${reminderTime}`;
-    reminderList.appendChild(li);
-    reminderForm.reset();
-}
+function placeBid() {
+    alert("Successfully Bid!");
+  }
+  const Buttons = 
+    document.querySelectorAll(".art-card button");
+  Buttons.forEach(button => {
+    button.addEventListener("click", placeBid);
+  });
+  
