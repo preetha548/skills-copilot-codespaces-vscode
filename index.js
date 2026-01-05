6ae89899268bbd8367f6269ed7cfa33a292d470b
preetha548 <vaishnavihiteash@gmail.com>
2026-01-05 13:57:06 +0530

    Scroll Animations

--- index.js
+++ index.js
@@ -1,36 +1,17 @@
-import { TaskStatus } from "../app.js";
+const boxes = document.querySelectorAll(".box");
 
-// render functions
-export function renderTask(task) {
-  const isCompleted = task.status === TaskStatus.COMPLETED;
+window.addEventListener("scroll", checkBoxes);
 
-  return `
-    <div class='task ${isCompleted ? "active" : ""}' id='${task.id}'>
-      <div class='toggle'>
-        ${isCompleted ? '<i class="fa fa-check"></i>' : ""}
-      </div>
-      <div class='title'>${task.title}</div>
-      <div class='delete-task'>
-        <i class='fa fa-trash'></i>
-      </div>
-    </div>
-  `;
-}
+function checkBoxes() {
+  const triggerBottom = (window.innerHeight / 5) * 4;
 
-export function renderFooter(taskList, filter) {
-  return `
-  <div class='task-list-footer ${!taskList.length ? "empty" : ""}'>
-    <div class='total-items'>${taskList.length} tasks</div>
-    <div class='filters'>
-        <div class='filter all ${filter === "all" ? "active" : ""}'>Tutti</div>
-        <div class='filter completed ${
-          filter === "completed" ? "active" : ""
-        }'>Completati</div>
-        <div class='filter uncompleted ${
-          filter === "todo" ? "active" : ""
-        }'>Da fare</div>
-      </div>
-      <div class='clear'>Cancella tutti</div>
-    </div>
-  `;
+  boxes.forEach((box) => {
+    const boxTop = box.getBoundingClientRect().top;
+
+    if (boxTop < triggerBottom) {
+      box.classList.add("show");
+    } else {
+      box.classList.remove("show");
+    }
+  });
 }
