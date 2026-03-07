822532ed74e08162f9b63361aae410d1d5d09c08
preetha548 <vaishnavihiteash@gmail.com>
2026-03-07 20:16:37 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,24 +1,4 @@
-// Date
-document.getElementById("date").textContent =
-    new Date().toDateString();
-
-// Breaking News
-document.getElementById("ticker").textContent =
-    "Global markets surge | Major sports final tonight | New AI breakthrough announced";
-
-// Dark Mode
-document.getElementById("themeToggle").onclick = () => {
-    document.body.classList.toggle("dark");
-};
-
-// News Filter
-function filterNews(category) {
-    const articles = document.querySelectorAll("article");
-    articles.forEach(article => {
-        if (category === "all" || article.classList.contains(category)) {
-            article.style.display = "block";
-        } else {
-            article.style.display = "none";
-        }
-    });
+function redeem(coupon)
+{
+alert("Coupon Redeemed: " + coupon + " ❤️");
 }
