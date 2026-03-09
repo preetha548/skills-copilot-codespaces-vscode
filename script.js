c9086cebd7b0cf4fb81e024b923dc29ea0563dd3
preetha vaishnavi <vaishnavihiteash@gmail.com>
2026-03-09 21:38:11 +0530

    Novel

--- script.js
+++ script.js
@@ -1,4 +1,23 @@
-function redeem(coupon)
-{
-alert("Coupon Redeemed: " + coupon + " ❤️");
+const container = document.getElementById("chapters");
+
+for(let i=1;i<=8;i++){
+
+let chapter = document.createElement("div");
+chapter.classList.add("chapter");
+
+chapter.innerHTML = `
+<h3>CHAPTER/SCENE:</h3>
+
+<p>POV:</p>
+<input type="text">
+
+<p>TIME:</p>
+<input type="text">
+
+<p>EVENTS:</p>
+<textarea></textarea>
+`;
+
+container.appendChild(chapter);
+
 }
