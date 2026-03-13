184e8d5305439433de067a4dd301b203fea980aa
preetha vaishnavi <vaishnavihiteash@gmail.com>
2026-03-13 16:26:18 +0530

    Eggless rosemilk cake

--- script.js
+++ script.js
@@ -1,14 +1,12 @@
-function changeMessage(){
+function showRecipe(){
 
-    const messages = [
-        "Have a wonderful day!",
-        "May all your dreams come true!",
-        "Stay happy and keep smiling!",
-        "Wishing you success and joy!",
-        "You are amazing!"
-    ];
+var recipe = document.getElementById("recipe");
 
-    let random = Math.floor(Math.random() * messages.length);
+if(recipe.style.display === "block"){
+recipe.style.display = "none";
+}
+else{
+recipe.style.display = "block";
+}
 
-    document.getElementById("message").innerText = messages[random];
 }
